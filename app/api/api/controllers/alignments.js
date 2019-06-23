f00e08941fd0adbadaec784937fe487be8a62077
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-06-22 23:43:34 -0400

    Add GET alignments/{id} api

--- app/api/api/controllers/alignments.js
+++ app/api/api/controllers/alignments.js
@@ -4,9 +4,9 @@ const uuid=require('uuid');
 const queue = require('../../queue/queue-service');
 const status = require('../../status/cache');
 
-function alignRequest(req, res) {
+function requestAlignment(req, res) {
   const request = req.swagger.params.request.value;
-  console.log(`Received request`, request);
+  console.log(`Received alignment request`, request);
   const sequence = request.sequence;
   const taskId = uuid.v4();
   const task = {
@@ -29,6 +29,19 @@ function alignRequest(req, res) {
   res.status(202).json(response);
 }
 
+function getAlignment(req, res) {
+  const id = req.swagger.params.id.value;
+  console.log(`Received alignment query`, id);
+  const response = {
+    id,
+    sequence: 'AATTCAG',
+    proteinName: 'AwesomeProtein',
+    proteinPosition: 27
+  };
+  res.status(200).json(response);
+}
+
 module.exports = {
-  alignRequest
+  requestAlignment,
+  getAlignment
 };
