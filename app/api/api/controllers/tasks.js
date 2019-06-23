dd396e8f023d21da4c87bf267cd3291a01fee14c
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-06-23 14:19:42 -0400

    Cleanup

--- app/api/api/controllers/tasks.js
+++ app/api/api/controllers/tasks.js
@@ -6,7 +6,7 @@ function requestStatus(req, res) {
   const taskId = req.swagger.params.taskId.value;
   const status = statusCache[taskId];
   if (status) {
-    res.status(200).json(status);
+    res.json(status);
   } else {
     res.status(404).json('Not found');
   }
