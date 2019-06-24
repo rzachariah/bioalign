2d7f54e1261a6002a44b31101b97cd6ea1981b06
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-06-24 17:38:14 -0400

    Version compute

--- app/compute/deploy.sh
+++ app/compute/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o verbose
 
-VERSION=1.0.0
+VERSION=1.0.1
 echo VERSION=$VERSION
 docker build -t bioalign_compute .
 docker tag bioalign_compute 848798434565.dkr.ecr.us-east-1.amazonaws.com/bioalign/compute:$VERSION
