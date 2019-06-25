e2c78eb09e3db5100dd45376cd412ef982dc45aa
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-06-25 00:24:43 -0400

    Uptick versions

--- app/api/deploy.sh
+++ app/api/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o verbose
 
-VERSION=1.0.6
+VERSION=$(npm version patch)
 echo VERSION=$VERSION
 docker build -t bioalign_api .
 docker tag bioalign_api 848798434565.dkr.ecr.us-east-1.amazonaws.com/bioalign/api:$VERSION
